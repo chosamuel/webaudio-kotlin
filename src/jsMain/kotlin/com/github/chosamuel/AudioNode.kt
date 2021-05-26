e01caafdfd64c482dcd292e813faf6040caf8cdb
Samuel Cho <cho.sam@icloud.com>
2021-05-26 16:14:08 +0200

    add remaining properties of AudioNode

--- src/jsMain/kotlin/com/github/chosamuel/AudioNode.kt
+++ src/jsMain/kotlin/com/github/chosamuel/AudioNode.kt
@@ -2,8 +2,14 @@ package com.github.chosamuel
 
 import org.w3c.dom.events.EventTarget
 
-
 external open class AudioNode : EventTarget {
+    val context: BaseAudioContext
+    val numberOfInputs: Int
+    val numberOfOutputs: Int
+    var channelCount: Int
+    var channelCountMode: String
+    var channelInterpretation: String
+
     fun connect(target: AudioNode)
     fun disconnect()
 }
