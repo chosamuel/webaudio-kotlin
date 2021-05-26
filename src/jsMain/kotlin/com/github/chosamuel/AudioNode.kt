e6209258db294b73135cfc0bc9565602bec84e28
Samuel Cho <cho.sam@icloud.com>
2021-05-26 16:12:07 +0200

    let audionode inherit eventTarget

--- src/jsMain/kotlin/com/github/chosamuel/AudioNode.kt
+++ src/jsMain/kotlin/com/github/chosamuel/AudioNode.kt
@@ -1,7 +1,9 @@
 package com.github.chosamuel
 
+import org.w3c.dom.events.EventTarget
 
-external open class AudioNode {
+
+external open class AudioNode : EventTarget {
     fun connect(target: AudioNode)
     fun disconnect()
 }
